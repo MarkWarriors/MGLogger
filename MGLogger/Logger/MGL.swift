5fcd9a9f7451c04d58c80f15f14f23f4c8f219f7
Marco Guerrieri <guerrieri@csp.it>
2018-05-28 17:08:18 +0200

    [FC] First commit. I think I need to write some documentation asap. But it works, and it's not bad

---
+++ MGLogger/Logger/MGL.swift
@@ -0,0 +1,152 @@
+//
+//  MGL.swift
+//  MGL
+//
+//  Created by Marco Guerrieri on 28/05/18.
+//  Copyright © 2018 Marco Guerrieri. All rights reserved.
+//
+
+
+import Foundation
+
+public class MGL {
+    
+    public private(set) static var logPrefix : String = "MGLogger"
+    public private(set) static var logLevel : LogLevel = .noLog
+    public private(set) static var logCategories : [LogCategory] = []
+    public private(set) static var logLevelsForCategories : [LogLevel:[LogCategory]] = [:]
+    
+    public private(set) static var defaultLevel : LogLevel = .verbose
+    public private(set) static var defaultCategory : LogCategory = .notSpecified
+    public private(set) static var logType : LogType = .noLog
+    
+    private static var showCategoriesInLog : Bool = true
+    private static var showLevelInLog : Bool = true
+    
+   
+    
+    public enum LogType: Int16 {
+        case noLog = 0
+        case all = 1
+        case byLevel = 2
+        case byCategories = 3
+        case byLevelInCategories = 4
+        case byLevelsForCategories = 5
+    }
+    
+    public enum LogLevel: Int16 {
+        case noLog = 0
+        case error = 1
+        case warning = 2
+        case info = 3
+        case verbose = 4
+    }
+    
+    public enum LogCategory: String {
+        case notSpecified = "NOTSPECIFIED"
+        case custom = "CUSTOM"
+        case connection = "CONNECTION"
+        case error = "ERROR"
+        case warning = "WARNING"
+        case info = "INFO"
+        case user = "USER"
+        case login = "LOGIN"
+        case interaction = "INTERACTION"
+        case UI = "UI"
+        case debug = "DEBUG"
+        case timing = "TIMING"
+        case thread = "THREAD"
+        case queue = "QUEUE"
+    }
+    
+    
+    public static func setLogStyle(prefix: String, showCategories: Bool, showLevel: Bool){
+        self.logPrefix = prefix
+        self.showCategoriesInLog = showCategories
+        self.showLevelInLog = showLevel
+    }
+    
+    public static func setLogDefaultLevel(_ level: LogLevel){
+        self.defaultLevel = level
+    }
+    
+    public static func setLogDefaultCategory(_ category: LogCategory){
+        self.defaultCategory = category
+    }
+        
+    public static func showLogWithCategoryIn(_ categories: [LogCategory]) {
+        self.logCategories = categories
+        self.logType = .byCategories
+    }
+    
+    public static func showLogWithMinimumLevel(_ level: LogLevel) {
+        self.logLevel = level
+        self.logType = .byLevel
+    }
+    
+    public static func showLogWithMinimumLevel(_ level: LogLevel, inCategories categories: [LogCategory]) {
+        self.logLevel = level
+        self.logCategories = categories
+        self.logType = .byLevelInCategories
+    }
+    
+    public static func showLogWithSpecificLevelForCategories(_ array: [LogLevel:[LogCategory]]){
+        self.logLevelsForCategories = array
+        self.logType = .byLevelsForCategories
+    }
+    
+    
+    public static func log(_ text: String, level: LogLevel? = defaultLevel, category: LogCategory? = defaultCategory) {
+        if level != .noLog {
+            self.log(text, level: level ?? defaultLevel, categories: [category ?? defaultCategory])
+        }
+    }
+        
+    public static func log(_ text: String, level: LogLevel?, categories: [LogCategory]?) {
+        let level = level ?? defaultLevel
+        let categories = categories ?? [defaultCategory]
+        
+        if level != .noLog {
+            var log : Bool = false
+            
+            switch logType {
+            case .noLog:
+                break
+            case .all:
+                log = true
+                break
+            case .byLevel:
+                log = level.rawValue <= self.logLevel.rawValue
+                break
+            case .byCategories:
+                log = self.logCategories.contains { (category) -> Bool in
+                    return categories.contains(category)
+                }
+                break
+            case .byLevelInCategories:
+                log = level.rawValue <= self.logLevel.rawValue && self.logCategories.contains { (category) -> Bool in
+                    return categories.contains(category)
+                }
+                break
+            case .byLevelsForCategories:
+                for lXc in self.logLevelsForCategories {
+                    if level.rawValue <= lXc.key.rawValue && lXc.value.contains { (category) -> Bool in
+                        return categories.contains(category)
+                        } {
+                        log = true
+                        break
+                    }
+                }
+            }
+            
+            if log {
+                printLog(text, level: level, categories: categories)
+            }
+        }
+    }
+    
+    private static func printLog(_ text: String, level: LogLevel, categories: [LogCategory]){
+        print("\(self.showLevelInLog || self.showCategoriesInLog ? "\(self.logPrefix) " : "\(self.logPrefix)")\(self.showLevelInLog ? "[\(String(describing: level).uppercased())]": "")\(self.showCategoriesInLog ? "[\(categories.map{String(describing: $0).uppercased()}.joined(separator: "|"))]": ""): \(text)")
+    }
+    
+}
---
+++ MGLogger/ViewController.swift
@@ -0,0 +1,159 @@
+//
+//  ViewController.swift
+//  MGLogger
+//
+//  Created by Marco Guerrieri on 28/05/18.
+//  Copyright © 2018 Marco Guerrieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("------ LOG VERBOSE")
+        MGL.log("It Log!", level: .error)
+        
+        print("\n---- CHANGE STYLE, REMOVE LEVEL AND CATEGORIES")
+        MGL.setLogStyle(prefix: "Prefix", showCategories: false, showLevel: false)
+        MGL.log("It Log!", level: .verbose, category: .notSpecified)
+        
+        print("\n---- CHANGE STYLE, REMOVE LEVEL")
+        MGL.setLogStyle(prefix: "Prefix", showCategories: true, showLevel: false)
+        MGL.log("It Log!", level: .verbose, category: .notSpecified)
+        
+        print("\n---- CHANGE STYLE, REMOVE CATEGORIES")
+        MGL.setLogStyle(prefix: "Prefix", showCategories: false, showLevel: true)
+        MGL.log("It Log!", level: .verbose, category: .notSpecified)
+        
+        
+        MGL.setLogStyle(prefix: "Prefix", showCategories: true, showLevel: true)
+        print("\n\n-------- LOG WITH LEVEL")
+        print("---- miniumum level WARNING")
+        MGL.showLogWithMinimumLevel(.warning)
+        testLogLevels()
+        print("---- miniumum level VERBOSE")
+        MGL.showLogWithMinimumLevel(.verbose)
+        testLogLevels()
+        
+        
+        print("\n\n-------- LOG WITH CATEGORIES")
+        print("---- category CUSTOM")
+        MGL.showLogWithCategoryIn([.custom])
+        testLogCategories()
+        print("\n---- category NOTSPECIFIED")
+        MGL.showLogWithCategoryIn([.notSpecified])
+        testLogCategories()
+        print("\n---- category CUSTOM and NOTSPECIFIED")
+        MGL.showLogWithCategoryIn([.notSpecified, .custom])
+        testLogCategories()
+        
+        
+        print("\n\n-------- LOG WITH LEVEL AND CATEGORIES")
+        print("\n---- WARNING with category NOTSPECIFIED")
+        MGL.showLogWithMinimumLevel(.warning, inCategories: [.notSpecified])
+        testLogLevelsAndCategories()
+        
+        print("\n---- WARNING with category CUSTOM")
+        MGL.showLogWithMinimumLevel(.warning, inCategories: [.custom])
+        testLogLevelsAndCategories()
+        
+        print("\n---- WARNING with category [CUSTOM, NOTSPECIFIED]")
+        MGL.showLogWithMinimumLevel(.warning, inCategories: [.notSpecified, .custom])
+        testLogLevelsAndCategories()
+        
+        print("\n---- WARNING with category NOTSPECIFIED")
+        MGL.showLogWithMinimumLevel(.verbose, inCategories: [.notSpecified])
+        testLogLevelsAndCategories()
+        
+        print("\n---- WARNING with category CUSTOM")
+        MGL.showLogWithMinimumLevel(.verbose, inCategories: [.custom])
+        testLogLevelsAndCategories()
+        
+        print("\n---- WARNING with category [CUSTOM, NOTSPECIFIED]")
+        MGL.showLogWithMinimumLevel(.verbose, inCategories: [.notSpecified, .custom])
+        testLogLevelsAndCategories()
+        
+        
+        print("\n\n-------- LOG with LEVEL FOR CATEGORIES")
+        print("\n---- level: WARNING, category: NOTSPECIFIED | level: VERBOSE, category: CUSTOM")
+        MGL.showLogWithSpecificLevelForCategories(
+            [.warning : [.notSpecified],
+             .verbose : [.custom]])
+        testLogLevelsAndCategories()
+        print("\n---- level: WARNING, category: NOTSPECIFIED | level: NOLOG, category: CUSTOM")
+        MGL.showLogWithSpecificLevelForCategories(
+            [.warning : [.notSpecified],
+             .noLog : [.custom]])
+        testLogLevelsAndCategories()
+        print("\n---- level: ERROR, category: [CUSTOM, NOTSPECIFIED]")
+        MGL.showLogWithSpecificLevelForCategories(
+            [.error : [.notSpecified, .custom]])
+        testLogLevelsAndCategories()
+
+        
+    }
+    
+    private func testLogLevels(){
+        MGL.log("It Log!1", level: .verbose, categories: [])
+        MGL.log("It Log!2", level: .info, categories: [])
+        MGL.log("It Log!3", level: .warning, categories: [])
+        MGL.log("It Log!4", level: .error, categories: [])
+        MGL.log("It Log!5", level: .noLog, categories: [])
+    }
+    
+    private func testLogCategories() {
+        MGL.log("It Log!1", level: .verbose, categories: [])
+        MGL.log("It Log!2", level: .verbose, categories: [.notSpecified])
+        MGL.log("It Log!3", level: .verbose, categories: [.custom])
+        MGL.log("It Log!4", level: .verbose, categories: [.interaction])
+        MGL.log("It Log!5", level: .verbose, categories: [.notSpecified, .custom, .interaction])
+    }
+    
+    private func testLogLevelsAndCategories(){
+        print("-- Not specifing category --")
+        testLogLevels()
+        
+        print("-- Category notSpecified --")
+        MGL.log("It Log!1", level: .verbose, category: .notSpecified)
+        MGL.log("It Log!2", level: .info, category: .notSpecified)
+        MGL.log("It Log!3", level: .warning, category: .notSpecified)
+        MGL.log("It Log!4", level: .error, category: .notSpecified)
+        MGL.log("It Log!5", level: .noLog, category: .notSpecified)
+        
+        print("-- Category custom --")
+        MGL.log("It Log!1", level: .verbose, category: .custom)
+        MGL.log("It Log!2", level: .info, category: .custom)
+        MGL.log("It Log!3", level: .warning, category: .custom)
+        MGL.log("It Log!4", level: .error, category: .custom)
+        MGL.log("It Log!5", level: .noLog, category: .custom)
+        
+        print("-- Category array with only notSpecified --")
+        MGL.log("It Log!1", level: .verbose, categories: [.notSpecified])
+        MGL.log("It Log!2", level: .info, categories: [.notSpecified])
+        MGL.log("It Log!3", level: .warning, categories: [.notSpecified])
+        MGL.log("It Log!4", level: .error, categories: [.notSpecified])
+        MGL.log("It Log!5", level: .noLog, categories: [.notSpecified])
+        
+        print("-- Category array with only custom --")
+        MGL.log("It Log!1", level: .verbose, categories: [.custom])
+        MGL.log("It Log!2", level: .info, categories: [.custom])
+        MGL.log("It Log!3", level: .warning, categories: [.custom])
+        MGL.log("It Log!4", level: .error, categories: [.custom])
+        MGL.log("It Log!5", level: .noLog, categories: [.custom])
+        
+        print("-- Category array with only not specified and custom --")
+        MGL.log("It Log!1", level: .verbose, categories: [.notSpecified, .custom])
+        MGL.log("It Log!2", level: .info, categories: [.notSpecified, .custom])
+        MGL.log("It Log!3", level: .warning, categories: [.notSpecified, .custom])
+        MGL.log("It Log!4", level: .error, categories: [.notSpecified, .custom])
+        MGL.log("It Log!5", level: .noLog, categories: [.notSpecified, .custom])
+        
+        
+    }
+    
+    
+}
+
